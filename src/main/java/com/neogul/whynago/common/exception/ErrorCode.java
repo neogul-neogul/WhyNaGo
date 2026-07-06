f8ba6b39f712e40571f3967562527dfc3716cbcf
yerim <na990303@gmail.com>
2026-07-06 20:51:02 +0900

    feat: 공통 예외 처리 구조 구현

---
+++ src/main/java/com/neogul/whynago/common/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.neogul.whynago.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus status();
+    String code();
+    String message();
+}
