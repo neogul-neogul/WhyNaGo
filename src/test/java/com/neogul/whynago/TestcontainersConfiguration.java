0c6233ea2546abf30046cd8cfc3492987aa61a36
wlsh44 <k87913j@gmail.com>
2026-07-13 12:36:15 +0900

    chore: configure persistence profiles

--- src/test/java/com/neogul/whynago/TestcontainersConfiguration.java
+++ src/test/java/com/neogul/whynago/TestcontainersConfiguration.java
@@ -1,8 +1,17 @@
 package com.neogul.whynago;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
+    @Bean
+    @ServiceConnection
+    public MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+    }
 }
